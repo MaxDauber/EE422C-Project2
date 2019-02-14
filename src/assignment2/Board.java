6fed7884db80b992d8e9243c5c1b70c02d1efb9b
Max Dauber <max@dauberfamily.com>
2019-02-14 17:21:53 -0600

    added checkValidInput and most of runGame
    finish checking of white pegs (correct colot in wrong spots)

--- src/assignment2/Board.java
+++ src/assignment2/Board.java
@@ -13,20 +13,29 @@ public class Board {
     public int numTurns;
     public String[] guesses;
     public Peg[] feedback;
+    public int turn;
 
     public Board(int numTurns){
         this.numTurns = numTurns;
         guesses = new String[GameConfiguration.guessNumber];
         feedback = new Peg[GameConfiguration.guessNumber];
+        turn  = 0;
     }
 
+    public void addGuess(String guess, Peg result){
+        guesses[turn] = guess;
+        feedback[turn] = result;
+        turn++;
+    }
+
+    @Override
     public String toString(){
         String ret = "";
-        for(int k = 0; k < guesses.length; k++){
+        for(int k = 0; k < turn; k++){
             ret += guesses[k];
             ret += " -> ";
             ret += feedback[k];
-            ret += "b_\n";
+            ret += "\n";
         }
         return ret;
     }
--- src/assignment2/Game.java
+++ src/assignment2/Game.java
@@ -27,10 +27,55 @@ public class Game {
         board  = new Board(GameConfiguration.guessNumber);
     }
     public void runGame(){
+        if(mode){
+            System.out.println("Secret code: " + secret_code);
+        }
+        System.out.println();
         for(int num = board.numTurns; num > 0; num--){
-            System.out.println("You have " + num + " guess(es) left.");
+            System.out.println("You have " + num + " guess(es) left");
+            System.out.println("Enter guess: ");
+            String guess = scan.next();
+            while(!checkValidInput(guess)){
+                System.out.println("INVALID_GUESS\n");
+                System.out.println("You have " + num + " guess(es) left");
+                System.out.println("Enter guess: ");
+                guess = scan.next();
+            }
+
+            Peg result = checkInput(guess);
+            board.addGuess(guess, result);
+            System.out.println(board.toString());
+
 
         }
     }
+
+    public boolean checkValidInput(String guess){
+        if(guess.length() != GameConfiguration.pegNumber){
+            return false;
+        }
+        boolean check;
+        for(int i = 0; i < guess.length(); i++){
+            check = false;
+            for(int j = 0; j < GameConfiguration.colors.length; j++){
+                if(guess.substring(i,i+1).equals(GameConfiguration.colors[j]))
+                    check = true;
+            }
+            if(!check)
+                return false;
+        }
+        return true;
+    }
+
+    public Peg checkInput(String input){
+        int black = 0;
+        int white = 0;
+        for(int i  = 0; i < GameConfiguration.pegNumber; i++) {
+            if(input.substring(i,i+1).equals(secret_code.substring(i,i+1)))
+                black++;
+        }
+        return new Peg(black, white);
+    }
+
 
 }
